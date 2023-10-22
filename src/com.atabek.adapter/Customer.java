f8913992face996bd13b22437869d0a669d67478
Atabek <otabek.shadimatov@gmail.com>
2023-10-22 17:42:45 +0600

    first commit

---
+++ src/com.atabek.adapter/Customer.java
@@ -0,0 +1,7 @@
+package com.atabek.adapter;
+
+public interface Customer {
+	public String getName();
+	public int getAge();
+	public Address getAddress();
+}
---
+++ src/com.atabek.adapter/Main.java
@@ -0,0 +1,15 @@
+package com.atabek.adapter;
+
+public class Main {
+	public static void main(String[] args) {
+		OldCustomer oldCustomer = new OldCustomer();
+		oldCustomer.setName("Tom");
+		oldCustomer.setAge("30");
+		oldCustomer.setAddress("GoldStreet,30,20021,New York");
+		
+		Customer customer = new CustomerAdapter(oldCustomer);
+		System.out.println(customer.getName());
+		System.out.println(customer.getAge());
+		System.out.println(customer.getAddress());
+		}
+}
